2b69c3ff6768e23035ac2799998712b5f98686ea
lybell <32293736+lybell-art@users.noreply.github.com>
2023-11-25 11:00:42 +0900

    Deploy 1.1.0

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const unityConfig = {
 	streamingAssetsUrl: "StreamingAssets",
 	companyName: "Lybell",
 	productName: "Guidance",
-	productVersion: "1.0.2",
+	productVersion: "1.1.0",
 	// matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
 	// devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
 };
